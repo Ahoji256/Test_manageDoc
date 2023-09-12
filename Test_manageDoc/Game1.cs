9861d654828c273c2be02b9cb0f198e7ed419469
DESKTOP-325D3AA\Mojibing <gtmkf123@gmail.com>
2023-09-12 15:24:44 +0700

    Add int jiji.

--- Test_manageDoc/Game1.cs
+++ Test_manageDoc/Game1.cs
@@ -9,6 +9,8 @@ namespace Test_manageDoc
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        public int jiji = 56;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
